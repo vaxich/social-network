e0374a9e6ef345a2b6e9fadf3ee792842128b704
vaxich <vaxich@rambler.ru>
2022-02-25 18:44:10 +0300

    42 lesson

---
+++ my-app/src/components/Dialogs/DialogsContainer.tsx
@@ -0,0 +1,58 @@
+import React, {ChangeEvent, ChangeEventHandler} from "react";
+import style from "./Dialogs.module.css";
+import {NavLink} from 'react-router-dom';
+import Message, {MessagePropsType} from "./Message/Message";
+import DialogItem from "./DialogItem/DialogItem";
+import {dialogsType, messageType} from "../Redux/store";
+import {sendMessageCreator, updateNewMessageCreator} from "../Redux/dialogs-reduser";
+
+
+type DialogsPropsType = {
+
+    store: any
+
+}
+
+
+const Dialogs = (props: DialogsPropsType) => {
+    let state = props.store.getState().dialogsPage;
+
+    let dialogsElement = state.dialogs.map((dialog: dialogsType) => <DialogItem name={dialog.name} id={dialog.id}/>);
+    let messageElements = state.messages.map((message: messageType) => <Message message={message.message}/>);
+    let newMessageBody = state.newMessageBody;
+    const onSendMessageClick = ( ) => {
+
+        props.store.dispatch(sendMessageCreator(newMessageBody))
+    }
+    const onNewMessageChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        let body = e.currentTarget.value
+       props.store.dispatch(updateNewMessageCreator(body))
+        console.log(body)
+    }
+    return (
+        <div className={style.dialogs}>
+            <div className={style.dialogs__item}>
+                {dialogsElement}
+
+
+            </div>
+            <div className={style.messages}>
+                <div>{messageElements}</div>
+                <div>
+                    <div><textarea
+                        value={newMessageBody}
+                        onChange={onNewMessageChange}
+                        placeholder="введите Ваше сообщение">
+
+                    </textarea></div>
+                    <div>
+                        <button onClick={onSendMessageClick}>SEND</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Dialogs;
---
+++ my-app/src/components/Profile/MyPosts/MyPostsContainer.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import {postType} from "../../Redux/store";
+import style from "./MyPosts.module.css";
+import Post from "./Post/Posts";
+import {type} from "os";
+import {addPostActionCreator, updateNewPostTextActionCreator} from "../../Redux/profile-reduser";
+
+type newPostElementTypr = any
+
+type MyPostsPropsType = {
+    posts: Array<postType>
+    newPostText: string
+    dispatch: any
+
+}
+
+
+const MyPosts = (props: MyPostsPropsType) => {
+
+    let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>);
+
+    let newPostElement: any = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        //props.addPost(text);
+        //props.updateNewPostText("")
+        props.dispatch(addPostActionCreator());
+    }
+
+
+    let onPostChange = () => {
+        let text = newPostElement.current.value;
+        //props.updateNewPostText(text)
+        let action = updateNewPostTextActionCreator(text);
+        props.dispatch(action);
+
+    }
+
+    return (
+        <div>
+            <div>
+                <h3> My posts</h3>
+            </div>
+            <div>
+                <div>
+                    <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText}/>
+                </div>
+                <div>
+                    <button onClick={addPost}>Add post</button>
+                </div>
+            </div>
+            <div className={style.description__block}>
+                {postsElements}
+            </div>
+        </div>
+
+
+    )
+}
+
+export default MyPosts;
